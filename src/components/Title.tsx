b6de304b2395b653e478f54c71e83bab58a5ba03
withoutspaces <dms@ic.ufal.br>
2025-08-02 16:59:25 -0300

    chore: traduz o título para pt-br pra evitar bugs do tradutor

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ export function Title() {
     <div className="flex items-center gap-2">
       <img src="./key.png" alt="" className="w-10" />
       <h1 className="text-2xl sm:text-3xl text-blue-900 font-black tracking-tighter items-start">
-        Password.Generator
+        Gerador de Senhas
       </h1>
     </div>
   );
