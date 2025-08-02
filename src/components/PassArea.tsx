ebeaafadd7a039997597e02952d96ff8e286a19b
withoutspaces <dms@ic.ufal.br>
2025-08-02 17:01:06 -0300

    style: ajusta valores de padding para layouts responsivos

--- src/components/PassArea.tsx
+++ src/components/PassArea.tsx
@@ -3,7 +3,7 @@ type PassAreaProps = React.HTMLAttributes<HTMLButtonElement>;
 export function PassArea({ children, ...rest }: PassAreaProps) {
   return (
     <button
-      className="w-full bg-blue-900 h-12 p-4 font-semibold flex justify-center items-center rounded-md cursor-pointer border-2 border-blue-800"
+      className="w-full bg-blue-900 h-16 p-4 font-semibold flex justify-center items-center rounded-md cursor-pointer border-2 border-blue-800"
       {...rest}
     >
       {children}
